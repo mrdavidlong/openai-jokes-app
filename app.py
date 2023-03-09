3031da2605a7dd7a221b505b646f70727a4d8eb3
David Long <dhslong@gmail.com>
2023-03-08 22:04:37 -0800

    added files

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        topic = request.form["topic"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Tell me a joke about {}".format(topic.capitalize())}
+            ],
+            temperature=1.5,
+        )
+        return redirect(url_for("index", result=response.choices[0].message.content))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
